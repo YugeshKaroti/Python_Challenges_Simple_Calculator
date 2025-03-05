0e2e8696d700ba5e50b05b289a0028a9b2c26f5b
YugeshKaroti <137762434+YugeshKaroti@users.noreply.github.com>
2025-03-05 11:07:17 +0530

    validator.py

--- modules/validator.py
+++ modules/validator.py
@@ -1,7 +1,7 @@
 def is_valid_expression(expression : str) -> bool:
     '''To check whether the expression is valid or not'''
     import re
-    if re.match("[\(]?\d+[\s]?[+|\-|*|\/][\s]?\d+[\)]?", expression):
+    if re.match("(\(?\d+[+|\-|*|\/].+\)?)|\(?\d\s?[+|\-|*|\/].+", expression):
         return True
     else:
         return False
